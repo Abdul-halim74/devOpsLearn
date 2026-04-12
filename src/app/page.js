80c85f77a96e23e75e2d4ccf3ff2df1f20651e4f
Abdul Halim <halimkhanfeni7@gmail.com>
2026-04-12 23:48:46 +0600

    second

---
+++ src/app/page.js
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <h1 className="text-6xl font-bold text-center sm:text-left">
+          Welcome to{" "}
+          <a href="https://nextjs.org" className="text-blue-600">
+            Next.js!
+          </a>
+        </h1>
+        <p className="mt-3 text-2xl text-center sm:text-left">
+          Get started by editing{" "}
+          <code className="bg-gray-100 rounded-md p-3 font-mono text-lg">
+            src/app/page.js
+          </code>
+          I am halim
+        </p>
+      </main>
+    </div>
+  );
+}
